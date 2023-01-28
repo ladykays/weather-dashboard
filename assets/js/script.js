df8e3d9e6fd212700b82aa56ecd1b3467495fc1f
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-28 22:43:44 +0000

    update code structure to make it more readable. Section fully functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,94 +29,81 @@ $(document).ready(function () { // Instructs the browser to only load script fil
 
   */
 
-  // Create a functionality to get the date and time using moment.js
-  var date = moment().format("DD/MM/YYYY");
-  console.log("Current Date: " + date);
-
-  // Create a div for displaying the current date and append it to the page
-  date = $("<h2>")
-    .addClass("date").text("( " + date + " )");
-  $(".current").append(date);
+  
 
 
-
-
-  // Search button event listener
-  $("button").on("click", function (event) {
-    event.preventDefault();
+  // Build the URL for querying the weather API
+  function buildQueryURL() {
     var city = $("#search-input")
       .val()
       .trim();
     var apiKey = "430a3842b09d883ae73a59e0c1d18fa2"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + apiKey; // use forecast endpoint instead of weather endpoint for a 5 day forcast. Also, add cnt=5 to get 5 day forecast
+    return queryURL;   
+  }
+  console.log("Query URL: " + buildQueryURL());
 
-    // Build the URL for querying the weather API
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + apiKey; // use forecast endpoint instead of weather endpoint for a 5 day forcast. Also, add cnt=5 to get 5 day forecast
+
+  function updatePage(weatherData) {
+    var date = moment().format("DD/MM/YYYY"); // Get the current date using moment.js and store it in a variable called date
+    console.log("Current Date: " + date);
+
+    // Assign weather data to variables
+    var cityName = weatherData.city.name;
+    var weatherIcon = weatherData.list[0].weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"; // Weather icon URL
+    var tempK = weatherData.list[0].main.temp;
+    var humidity = weatherData.list[0].main.humidity;
+    var windSpeed = weatherData.list[0].wind.speed;
+
+    // Create elements for displaying weather data
+    //=============================================== 
+    // Element for displaying the current date
+    date = $("<h2>")
+      .addClass("date")
+      .text("( " + date + " )"); 
+    cityName = $("<h2>").addClass("city-name").text(cityName); 
+    var weatherIconEl = $("<img>").addClass("weather-icon").attr("src", iconURL);
+    var tempC = (tempK - 273.15).toFixed(2); // Convert Kelvin to Celsius
+    var tempEl = $("<p>").addClass("temp").text("Temperature: " + tempC + " °C");
+    var humidityEl = $("<p>").addClass("humidity").text("Humidity: " + humidity);
+    var windSpeedEl = $("<p>").addClass("wind-speed").text("Wind Speed:  " + windSpeed + " mph");
+
+    // Add the newly created elements to the DOM
+    $(".current").append(date);
+    $(".current").prepend(cityName);  // Prepending after emptying the element will ensure that only one city name is displayed at a time
+    $(".current").append(weatherIconEl);
+    $(".current").append(tempEl);
+    $(".current").append(humidityEl);
+    $(".current").append(windSpeedEl);
+    
+  }
 
 
+  // Function to remove the weather data
+  function clearWeather() {
+    $(".date").remove();
+    $(".city-name").remove();
+    $(".weather-icon").remove();
+    $(".temp").remove();
+    $(".humidity").remove();
+    $(".wind-speed").remove();
+  }
+
+
+  // CLICK HANDLER
+  //==============================
+  // Search button event listener
+  $("#search-button").on("click", function (event) {
+    event.preventDefault();
+    clearWeather();
+    
+    var queryURL = buildQueryURL();
     // Make an AJAX call to the openweather API
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function (response) {
-      console.log("Weather Information: " + response);
-      console.log(queryURL);
-      console.log(response);
-      //$(".current").text(JSON.stringify(response));
-
-      
-      //Display the weather data for the current date on the page.
-      var cityName = response.city.name;
-      cityName = $("<h2>").addClass("city-name").text(cityName); 
-      console.log("City: " + cityName);
-
-      $(".city-name").empty(); // Empty the city name element
-      $(".current").prepend(cityName);  // Prepending after emptying the element will ensure that only one city name is displayed at a time
-
-
-      // Get weather icon and display it on the page
-      var weatherIcon = response.list[0].weather[0].icon;
-      var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-      console.log(iconURL);
-      var weatherIconEl = $("<img>").addClass("weather-icon").attr("src", iconURL);
-
-      $(".weather-icon").remove();
-      //$(".current").empty(); 
-      // TODO: Try to use the commented out code above to remove every element from the div 
-      // TODO: Try to change the color of the weather icon
-      $(".current").append(weatherIconEl);
-
-      
-      // Get the temperature and display it on the page
-      var tempK = response.list[0].main.temp;
-      var tempC = (tempK - 273.15).toFixed(2);
-      var tempEl = $("<p>").addClass("temp").text("Temperature: " + tempC + " °C");
-
-      $(".temp").remove(); // Empty the temp element
-      $(".current").append(tempEl);
-
-       // Get the humidity and display it on the page
-      var humidity = response.list[0].main.humidity;
-      var humidityEl = $("<p>").addClass("humidity").text("Humidity: " + humidity);
-
-      $(".humidity").remove(); // Empty the humidity element
-      $(".current").append(humidityEl); 
-
-      //Get the wind speed and display it on the page
-      var windSpeed = response.list[0].wind.speed;
-      var windSpeedEl = $("<p>").addClass("wind-speed").text("Wind Speed: wind" + windSpeed + " mph");
-
-      $(".wind-speed").remove(); // Empty the wind speed element
-      $(".current").append(windSpeedEl);
-      console.log(windSpeed);
-
-    });
-    console.log(queryURL);
-
-  
-      
-
-
-
+    }).then(updatePage);
       
   });
 
