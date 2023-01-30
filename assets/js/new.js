723feee35f3b07b62955c7b6e6126477eee2e14e
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-30 21:35:32 +0000

    add function currentWeather

--- assets/js/new.js
+++ assets/js/new.js
@@ -28,22 +28,69 @@ $(document).ready(function () { // Instructs the browser to only load script fil
       console.log(response);
       var date = moment().format("DD/MM/YYYY"); // Get the current date using moment.js and store it in a variable called date
       console.log("Current Date: " + date);
-      data();
+      currentWeather();
 
-      function data() {
+      function currentWeather() {
         // Assign weather data to variables
         var cityName = response.name;
-        var country = response.country;
-        var weatherIcon = response.weather[0].icon;
-        var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"; // Weather icon URL
+        var country = response.sys.country;
+        var currentWeatherIcon = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png"; // Weather icon URL
         var tempK = response.main.temp;
         var humidity = response.main.humidity;
         var windSpeed = response.wind.speed;
 
-        console.log(cityName, country, weatherIcon, iconURL, tempK, humidity, windSpeed);
-      } // End of data function
+        console.log(cityName, country, currentWeatherIcon, iconURL, tempK, humidity, windSpeed);
+
+        // Create elements for displaying weather data
+        //=============================================== 
+        // Element for displaying the current date
+        var headingContainer = $("<div>")
+          .addClass("heading-container")
+          .prependTo(".current");
+        date = $("<h2>")
+          .addClass("date")
+          .text("( " + date + " )"); 
+        cityName = $("<h2>")
+          .addClass("city-name")
+          .text(cityName); 
+        country = $("<h2>")
+          .addClass("country")
+          .text(country);
+        var weatherIconEl = $("<img>")
+          .addClass("weather-icon")
+          .attr("src", iconURL);
+        var tempC = (tempK - 273.15).toFixed(2); // Convert Kelvin to Celsius
+        var tempEl = $("<p>")
+          .addClass("temp")
+          .text("Temperature: " + tempC + " °C");
+        var humidityEl = $("<p>")
+          .addClass("humidity")
+          .text("Humidity: " + humidity);
+        var windSpeedEl = $("<p>")
+          .addClass("wind-speed")
+          .text("Wind Speed:  " + windSpeed + " mph")
+
+        // Add the newly created elements to the DOM
+        //$(".current-weather-container").prepend(headingContainer);
+        $(cityName).appendTo(headingContainer);
+        $(country).appendTo(headingContainer);
+        $(date).appendTo(headingContainer);
+        $(weatherIconEl).appendTo(headingContainer);
+        
+        
+        $(".current").append(tempEl);
+        $(".current").append(humidityEl);
+        $(".current").append(windSpeedEl);
+      } // End of currentWeather function
+
+      // 5 Day Weather Function
+      function weatherForecast() {
+        
+      } // End of weatherForecast function
+
     }); // End function response
-  }); // End search button event listener
+  }); // End of click event function
 
 // Function to remove the weather data
 function clearWeather() {
