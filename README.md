9bf5cbf952bc90e65d88d3170bd85bfa529c2c86
Kerrion Siokwu <ladykerrion@sky.com>
2023-02-07 17:57:03 +0000

    bug-fix: dates for the 5-day forcast fixed

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * Normalize.CSS : https://byby.dev/normalize-css
 * Reset.CSS: https://www.joshwcomeau.com/css/custom-css-reset/normalize-css-or-css-reset-9d75175c5d1e
 * https://www.youtube.com/watch?v=4UoUqnjUC2c
+* https://www.youtube.com/watch?v=QEu8_5bYm-w
--- assets/js/latest.js
+++ assets/js/latest.js
@@ -3,11 +3,10 @@ $(document).ready(function () { // Instructs the browser to only load script fil
   var apiKey = "&appid=430a3842b09d883ae73a59e0c1d18fa2";
   var city = "London";
   var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-  var units = "&units=imperial";
 
   // Build the URL for querying the weather API
   function buildQueryURL() {
-    var queryURL = apiURL + city + apiKey + units;
+    var queryURL = apiURL + city + apiKey;
     return queryURL;   
   }
   console.log("Query URL: " + buildQueryURL());
@@ -17,10 +16,12 @@ $(document).ready(function () { // Instructs the browser to only load script fil
   // Search button event listener
   $("#search-button").on("click", function (event) {
     event.preventDefault();
-    clearWeather();
+    //clearWeather();
+    $("#today").empty();
     city = $("#search-input").val(); // Updates the value of the search input
+    searchHistory();
     queryURL = buildQueryURL();
-    // Make an AJAX call to the openweather API
+    // Make an AJAX call to the openweather API to get the current weather
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -31,7 +32,7 @@ $(document).ready(function () { // Instructs the browser to only load script fil
       currentWeather();
       weatherForecast();
 
-      // Function to get current weather
+      // Function to display current weather
       function currentWeather() {
         // Assign weather data to variables
         var cityName = response.name;
@@ -76,17 +77,18 @@ $(document).ready(function () { // Instructs the browser to only load script fil
         headingContainer.append(cityName, country, today, weatherIconEl);
         $(".current").append(tempEl, humidityEl, windSpeedEl);
         
-      } // End of currentWeather function
+      } // end currentWeather function
 
       //return response;
 
-      // 5 Day Weather Function
+      // Function for 5-day weather forecast
       function weatherForecast() {
         var forecastAPIURL = "https://api.openweathermap.org/data/2.5/forecast?lat=";
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         console.log(lat, lon);
-        var forecastQueryURL = forecastAPIURL + lat + "&lon=" + lon  + "&include=daily&cnt=5" + apiKey;
+        //var forecastQueryURL = forecastAPIURL + lat + "&lon=" + lon  + "&include=daily&cnt=5" + apiKey;
+        var forecastQueryURL = forecastAPIURL + lat + "&lon=" + lon  + "&include=daily" + apiKey; // removed "&cnt=5" so all data is returned
         console.log(forecastQueryURL);
 
         $.ajax({
@@ -95,6 +97,30 @@ $(document).ready(function () { // Instructs the browser to only load script fil
         }).then(function (forecastResponse) {
           console.log(forecastResponse);
             
+          
+
+          // Only get the dt_txt of "00:00:00" timestamp
+          /* for (var i = 0; i < forecastResponse.list.length; i++) {
+            if (forecastResponse.list[i].dt_txt.indexOf("00:00:00") !== -1) {
+              
+              var date = new Date(forecastResponse.list[i].dt_txt * 1000).toLocaleDateString(); 
+              console.log(date); 
+
+              var forecastData = $("<div>")
+              .addClass("forecast-data")
+              .appendTo(forecastContainerEl);
+
+              var h4El = $("<h4>")
+              .addClass("forecast-date")
+              //.appendTo(forecastContainer)
+              .text(date); 
+
+              forecastContainerEl.append(h4El);
+              
+            }
+            
+          } */
+
           var title5DayEl = $("<h2>")
             .addClass("heading-5-day")
             .text("5-Day Forecast");
@@ -103,13 +129,14 @@ $(document).ready(function () { // Instructs the browser to only load script fil
             .addClass("forecast-container");
 
           $("#forecast").append(title5DayEl, forecastContainerEl);
-
           for (var i = 0; i < 5; i++) {
-            // Multiply by 1000 because the timestamp in the list array is in seconds, and the Date constructor takes milliseconds
-            // 
-            var date = new Date(forecastResponse.list[i].dt * 1000).toLocaleDateString(); 
-            console.log(date);
+            // Multiply by 1000 to convert the Unix timestamp in seconds to miliseconds
+            //var date = new Date(forecastResponse.list[i].dt * 1000).toLocaleDateString(); 
+            var date = new Date((forecastResponse.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+            console.log(date);  
 
+            //var date = document.getElementsByClassName("forecast-date" + (i + 1)).innerHTML = weekDay[checkDay(i)];
+            //var date = weekDay[checkDay(i)];
             // Create elements for displaying current weather data
             //====================================================== 
             
@@ -123,14 +150,11 @@ $(document).ready(function () { // Instructs the browser to only load script fil
             var h4El = $("<h4>")
               .addClass("forecast-date")
               //.appendTo(forecastContainer)
-              .text(date);
+              .text(date); 
 
             var tempEl = $("<div>")
               .addClass("data-element")
               //.appendTo(forecastContainer);
-
-            var windSpeedEl = $("<div>")
-              .addClass("data-element");
 
             var humidityEl = $("<div>")
              .addClass("data-element");
@@ -143,7 +167,7 @@ $(document).ready(function () { // Instructs the browser to only load script fil
             // Append the newly created elements to the DOM
             
             //title5DayEl.append(forecastContainer);
-            forecastData.append(h4El, iconEl, tempEl, windSpeedEl, humidityEl);
+            forecastData.append(h4El, iconEl, tempEl, humidityEl);
             
 
             // Update the page with the newly created elements
@@ -153,30 +177,75 @@ $(document).ready(function () { // Instructs the browser to only load script fil
             var tempC = (tempK - 273.15).toFixed(2); // Converts Kelvin to Celsius and rounds it to 2 decimal places
             console.log(tempK);
             console.log(tempC);
-            var windSpeed = forecastResponse.list[i].wind.speed;
             var humidity = forecastResponse.list[i].main.humidity;
 
             title5DayEl.text("5-Day Forecast");
             iconEl.attr("src", iconURL);
             tempEl.text("Temp: " + tempC + " °C");
-            windSpeedEl.text("Wind Speed: " + windSpeed + " mph");
             humidityEl.text("Humidity: " + humidity);
             
-
-
 
             //TODO: Get only time that atarts at midnight 00:00:00
           } // End of for loop
           
         }); // End of function forecastResponse
+        
+        /* // Function to check for day 
+        var d = new Date();
+        var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        function checkDay(day) {
+          if (day + d.getDay() > 6) {
+            return day + d.getDay() - 7;
+          } else {
+            return day + d.getDay();
+          }
+        } // end function checkDay */
 
       } // End of weatherForecast function
 
     }); // End function response
   }); // End of click event function
 
+  // Search history function
+  function searchHistory() {
+    var cityName = $("#search-input").val();
+    var cities = [];
+    //cityName.toUpperCase();
+    
+    
+    // Create and append elements for saved search history
+    /* var historyContainerEl = $("<div>")
+      .addClass("history-container")
+      .appendTo("#list-group"); */
+
+    var ulEl = $("<ul>").addClass("history-list-group")
+    .appendTo("#history");
+
+    var liEl = $("<li>");
+      
+      
+
+    // Add city to history list
+    var citiesList = cities.length;
+    cities.push(cityName);
+    localStorage.setItem(cityName, cities);
+    //cities = JSON.parse(localStorage.getItem("cities"));
+    console.log(cities);
+    cities.forEach(function (cityName) {
+      liEl.addClass("list-group-item").text(cityName).appendTo(ulEl);
+    }); 
+    /* for (var i = 0; i < cities.length; i++) {
+      liEl.addClass("list-group-item").text(cityName).appendTo(ulEl);
+    } */
+    
+      
+    
+  } // End of saveHistory function
+  searchHistory();
   // Function to remove the weather data
   function clearWeather() {
+
   
-}  
+  } // End of function clearWeather
 }); // End of document ready function
